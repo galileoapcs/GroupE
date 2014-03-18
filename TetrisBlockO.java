2aacabec0ccad33209128c3a3f83c208dfab73dc
Lutimer <lutimer@gmail.com>
2014-03-18 10:39:13 -0700

    BLockO
    hi

--- TetrisBlockO.java
+++ TetrisBlockO.java
@@ -1,11 +1,11 @@
 import info.gridworld.grid.Location;
 
 import java.awt.Color;
-
-
+	
+	
 public class TetrisBlockO extends TetrisBlock
 {
-
+	
 	public TetrisBlockO()
 	{
 		super();
@@ -16,36 +16,37 @@ public class TetrisBlockO extends TetrisBlock
 		blocks.add(b);
 		blocks.add(c);
 	}
-	
-	public void rotate()
-	{
 		
-	}
-	
+		
 	public void moveDown()
 	{
 		move();
 		blocks.get(0).move();
+		blocks.get(2).move();
+		blocks.get(1).move();
+		
 	}
-	
-	public void moveLeft()
-	{
-		setDirection(270);
-		for (TetrisBug tb : blocks)
-		{
-			tb.setDirection(270);
-		}
-		move();
-	}
-	
-	public void moveRight()
-	{
+	public void moveRight() {
 		setDirection(90);
 		for (TetrisBug tb : blocks)
-		{
 			tb.setDirection(90);
+			if (blocks.get(1).canMove() && blocks.get(2).canMove()) {
+				blocks.get(1).move();
+				blocks.get(2).move();
+				move();
+				blocks.get(0).move();
+			}
+	}
+	public void moveLeft() {
+		setDirection(270);
+		for (TetrisBug tb : blocks)
+			tb.setDirection(270);
+			if (canMove() && blocks.get(0).canMove()) {
+				move();
+				blocks.get(0).move();
+				blocks.get(1).move();
+				blocks.get(2).move();
+				
 		}
-		move();
 	}
 }
-
