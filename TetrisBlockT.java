ba12bc59105efc070bbe01cd92558f6402e75f56
Lutimer <lutimer@gmail.com>
2014-03-24 10:29:12 -0700

    BlockT New
    the one that works

--- TetrisBlockT.java
+++ TetrisBlockT.java
@@ -22,18 +22,50 @@ public class TetrisBlockT extends TetrisBlock
 	}
 		
 	public boolean canMoveDown() {
-		
+		if(rotationPos==0){
 			return canMove() && blocks.get(1).canMove() && blocks.get(2).canMove();
-
-		
-		
+		}
+		else if(rotationPos==1 ){
+			return blocks.get(1).canMove();
+		}
+		else if(rotationPos==2 ){
+			return canMove() && blocks.get(1).canMove() && blocks.get(2).canMove();
+		}
+		else if(rotationPos==3 ){
+			return blocks.get(2).canMove();
+		}
+		return false;
 	}
 	public void moveDown()
 	{
-		move();
-		blocks.get(0).move();
-		blocks.get(2).move();
-		blocks.get(1).move();
+		if(rotationPos==0)
+		{
+			move();
+			blocks.get(0).move();
+			blocks.get(2).move();
+			blocks.get(1).move();
+		}
+		else if(rotationPos==1)
+		{
+			blocks.get(1).move();
+			move();
+			blocks.get(2).move();
+			blocks.get(0).move();
+		}
+		else if(rotationPos==2)
+		{
+			move();
+			blocks.get(1).move();
+			blocks.get(2).move();
+			blocks.get(0).move();
+		}
+		else if(rotationPos==3)
+		{
+			blocks.get(2).move();
+			move();
+			blocks.get(0).move();
+			blocks.get(1).move();
+		}
 		
 	}
 	public void moveRight() {
@@ -41,61 +73,126 @@ public class TetrisBlockT extends TetrisBlock
 		
 		for (TetrisBug tb : blocks)
 			tb.setDirection(90);
+		if(rotationPos==0)
+		{
 			if (blocks.get(1).canMove()) {
 				move();
 				blocks.get(1).move();
 				blocks.get(0).move();
 				blocks.get(2).move();		
-				
-				
 			}
+		}
+		else if(rotationPos==1)
+		{
+			if(blocks.get(2).canMove()){
+				blocks.get(2).move();
+				move();
+				blocks.get(1).move();
+				blocks.get(0).move();
+			}
+		}
+		else if(rotationPos==2)
+		{
+			if(blocks.get(2).canMove()){
+				blocks.get(2).move();
+				move();
+				blocks.get(1).move();
+				blocks.get(0).move();
+			}
+		}
+		else if(rotationPos==3)
+		{
+			if(canMove()){
+				move();
+				blocks.get(2).move();
+				blocks.get(0).move();
+				blocks.get(1).move();
+			}
+		}
+		
 	}
 	public void moveLeft() {
 		setDirection(270);
 		for (TetrisBug tb : blocks)
 			tb.setDirection(270);
-			if (blocks.get(2).canMove()) {
+		if(rotationPos==0)
+		{
+			if(blocks.get(2).canMove()) {
 				move();
 				blocks.get(2).move();
 				blocks.get(0).move();
 				blocks.get(1).move();
-				
+			}
+		}
+		else if(rotationPos==1)
+		{
+			if(canMove()){
+				move();
+				blocks.get(1).move();
+				blocks.get(0).move();
+				blocks.get(2).move();
+			}
+		}
+		else if(rotationPos==2)
+		{
+			if(blocks.get(1).canMove()){
+				blocks.get(1).move();
+				move();
+				blocks.get(2).move();
+				blocks.get(0).move();
+			}
+		}
+		else if(rotationPos==3)
+		{
+			if(blocks.get(1).canMove()){
+				blocks.get(1).move();
+				move();
+				blocks.get(2).move();
+				blocks.get(0).move();
+			}
 		}
 	}
 	public void rotate() {
 		Location nextLoc;	
+		Location nextLoc2;
 		if (rotationPos == 0) {
 			// only one block must move
-			nextLoc = new Location(blocks.get(1).getLocation().getRow() - 1,
-					blocks.get(1).getLocation().getCol() - 1);
-			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null) {
-				blocks.get(2).moveTo(nextLoc);
+			nextLoc = new Location(getLocation().getRow() +1,
+					getLocation().getCol());
+			nextLoc2 = new Location(getLocation().getRow(),
+					getLocation().getCol()+1);
+			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null && gr.isValid(nextLoc2) && gr.get(nextLoc) == null) {
+				blocks.get(1).moveTo(nextLoc);
+				blocks.get(2).moveTo(nextLoc2);
 				rotationPos = (rotationPos + 1)%4;// will be % 4 with 4 blocks
 			}
 		} 
-		if (rotationPos == 1) {
-			nextLoc = new Location(blocks.get(1).getLocation().getRow() - 1,
-					blocks.get(1).getLocation().getCol() - 1);
+		else if (rotationPos == 1) {
+			nextLoc = new Location(getLocation().getRow(),
+					getLocation().getCol() - 1);
 			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null) {
-				moveTo(nextLoc);
+				blocks.get(1).moveTo(nextLoc);
 				rotationPos = (rotationPos + 1)%4;
 						// will be % 4 with 4 blocks
 			}
 		} 
-		if (rotationPos == 2) {
-			nextLoc = new Location(blocks.get(1).getLocation().getRow() + 1,
-					blocks.get(1).getLocation().getCol() - 1);
+		else if (rotationPos == 2) {
+			nextLoc = new Location(getLocation().getRow() + 1,
+					getLocation().getCol());
 			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null) {
-				moveTo(nextLoc);
+				blocks.get(2).moveTo(nextLoc);
 				rotationPos = (rotationPos + 1)%4;
 						// will be % 4 with 4 blocks
 			}
 		}
-		if (rotationPos == 3) {
-			nextLoc = new Location(blocks.get(1).getLocation().getRow() + 1,
-					blocks.get(1).getLocation().getCol() - 1);
-			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null) {
-				moveTo(nextLoc);
+		else if (rotationPos == 3) {
+			nextLoc = new Location(getLocation().getRow() - 1,
+					getLocation().getCol() - 1);
+			nextLoc2 = new Location(getLocation().getRow() -1,
+					getLocation().getCol() + 1);
+			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null && gr.isValid(nextLoc2) && gr.get(nextLoc) == null) {
+				blocks.get(2).moveTo(nextLoc);
+				blocks.get(1).moveTo(nextLoc2);
 				rotationPos = (rotationPos + 1)%4;
 						// will be % 4 with 4 blocks
 			}
